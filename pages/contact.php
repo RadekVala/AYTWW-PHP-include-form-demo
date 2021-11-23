e9c3e220db6e5c4eb36e33d3c025768cb375a9b6
RadekVala <radek.www@gmail.com>
2021-11-23 08:14:39 +0000

    Initial commit from PHPSandbox

---
+++ pages/contact.php
@@ -0,0 +1,11 @@
+<form method="post">
+    <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+    <input name="email" type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <input class="btn btn-success" type="submit" value="Send">
+</form>
